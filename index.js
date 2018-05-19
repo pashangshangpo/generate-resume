bc33cf93a4ec7bc8f419828c2bd77375709e540a
爬上上坡 <pashangshangpo@gmail.com>
2018-05-19 12:26:19 +0800

    初始化配置信息

---
+++ index.js
@@ -0,0 +1,40 @@
+/**
+ * @file 入口文件
+ * @author pashangshangpo
+ * @createTime 2018年5月19日 上午11:38
+ */
+
+/**
+ * @start-def: convert: (config, outputDir) => undefined
+ *   templateName: String 模板名
+ *   config: Object
+ *     name: String 姓名
+ *     age: number 年龄
+ *     sex: String 性别
+ *     tel: String 手机号
+ *     email: String 邮箱
+ *     education: String 学历
+ *     expectCity: String 期望城市
+ *     expectedSalary: String 期望薪资
+ *     jobIntension: String 求职意向
+ *     currentState: String 当前状态 如: 在职, 求职中
+ *     personalWebsite: Array [Item] 个人网站
+ *       Item: Object
+ *         name: String 名称
+ *         url: String 网址
+ *     toIntroduceMyself: String 自我介绍
+ *     workExperience: Array [Item] 工作经验
+ *       Item: String
+ *     projectExperience: Array [Item] 项目经验
+ *       Item: String
+ *     works: Array [Item] 作品
+ *       Item: Object
+ *         name: String 名称
+ *         list: Array [Item] 作品列表
+ *           Item: String
+ *     outputFileName: String 输出的文件名
+ *   outputDir: String 输出目录路径
+ */
+const convert = (templateName, config, outputDir) => {
+  
+}
